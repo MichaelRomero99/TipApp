47bd542b4e546c9eb0534d4d4e046c4f0ad739e5
MichaelRomero99 <michaelromero99school@gmail.com>
2020-06-01 18:34:09 -0700

    Initial Commit

---
+++ TipApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipApp
+//
+//  Created by Michael Romero on 6/1/20.
+//  Copyright © 2020 Michael Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
